0fc3d007afbf671dc00d1c2da64bcfcd5e1c9e40
Bernd Maass <bernd.maass.ext@msg.group>
2024-12-02 15:49:07 +0100

    Cosmos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import { CommonModule } from '@angular/common'
+
+import { RouterOutlet } from '@angular/router';
+import * as Cosmos from "@azure/cosmos";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
-  imports: [MatTabsModule]
+  imports: [MatTabsModule, CommonModule, RouterOutlet]
 })
-export class AppComponent {
-  value = 'Schruefophon';
+
+
+
+
+export class AppComponent implements OnInit{
+  dataSource = [];  
+  displayedColumns: string[] = ['categoryName', 'description', 'name', 'price'];
+  ngOnInit(): void {
+    this.connect();
+  }
+
+  public async connect() {   
+    try {
+        const endpoint = "https://schruefer.documents.azure.com:443/";
+        const key = "ZE8r1ZNlJuL7o1F10F5NuPlJgJiC2TElldQycH2QCxIaZzkGcnxA5Za3URdElQM8ef66ctGmLNz1ACDbc9JuIA";
+        const client = new Cosmos.CosmosClient({endpoint: endpoint, key: key});
+        const database = "SampleDB";
+        const collection = "Items";
+        const db = client.database(database);
+        const container = db.container("SampleContainer");
+        await container.items
+        .query({
+            query: "SELECT * from c"
+        })
+        .fetchAll()
+        .then((response: any) => {
+          console.log(response);
+          this.dataSource = this.createData(response); 
+          console.log(this.dataSource);    
+      }) 
+    } catch(error) {
+        console.log(error);
+
+    }    
+  } 
+
+  public createData(response) {
+    let items = [];
+    response.resources.forEach(element => {
+      const entry: any = [];
+      entry.categoryName = element.categoryName;
+      entry.description = element.description;
+      entry.name = element.name;
+      entry.price = element.price;
+      items.push(entry);
+    });
+    return items;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from "./app.component";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { OverviewComponent } from './overview/overview.component'
+import { SettingsComponent } from './settings/settings.component'
+const routes: Routes = [
+  { path: '', redirectTo: 'overview', pathMatch: 'full' },
+  { path: 'overview', component: OverviewComponent },
+  { path: 'settings', component: SettingsComponent }
+];
+
 @NgModule({
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
   bootstrap: [AppComponent],
   providers: [
     provideAnimationsAsync()
